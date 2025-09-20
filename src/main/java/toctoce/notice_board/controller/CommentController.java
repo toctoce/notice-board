c544baacb2d415464bb7efe24eea96dddc052a93
toctoce <toctoce@naver.com>
2025-09-20 22:54:13 +0900

    Feat: 댓글 수정 기능 추가

--- src/main/java/toctoce/notice_board/controller/CommentController.java
+++ src/main/java/toctoce/notice_board/controller/CommentController.java
@@ -4,7 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import toctoce.notice_board.domain.Comment;
 import toctoce.notice_board.dto.CommentCreateRequestDto;
+import toctoce.notice_board.dto.CommentUpdateRequestDto;
 import toctoce.notice_board.service.CommentService;
 
 @Controller
@@ -19,4 +23,10 @@ public class CommentController {
         return "redirect:/posts/" + postId;
     }
 
+    @PutMapping("/comments/{commentId}")
+    public String editComment(CommentUpdateRequestDto dto, @PathVariable long commentId) {
+        commentService.updateComment(dto, commentId);
+        Comment comment = commentService.findOne(commentId);
+        return "redirect:/posts/" + comment.getPost().getId();
+    }
 }
